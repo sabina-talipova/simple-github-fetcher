876821bb39d0ca4309ed0a57d37efbd341f66520
Sabina Talipova <sabina.talipova@silverstripe.com>
2022-12-08 14:31:49 +1300

    Small fetcher update

--- fetch.php
+++ fetch.php
@@ -13,40 +13,46 @@ function get_credentials() {
             $token = str_replace('"', '', $a[1]);
         }
     }
-    return implode(':', [$user, $token]);
+    return implode(' ', [$user, $token]);
 }
 
 function fetch($url) {
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:28.0) Gecko/20100101 Firefox/28.0'
+    $curl_request = curl_init();
+    curl_setopt($curl_request, CURLOPT_URL, $url);
+    // curl_setopt($curl_request, CURLOPT_USERPWD, get_credentials());
+    curl_setopt($curl_request, CURLOPT_HTTPHEADER, [
+        'User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:28.0) Gecko/20100101 Firefox/28.0',
+        "Authorization: ". get_credentials() ."",
+        "Accept: application/vnd.github+json",
+        "X-GitHub-Api-Version: 2022-11-28"
     ]);
-    curl_setopt($ch, CURLOPT_USERPWD, get_credentials());
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+    curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl_request, CURLOPT_SSL_VERIFYPEER, false);
     sleep(1); // don't exceed rate limit
-    $s = curl_exec($ch);
-    curl_close($ch);
-    return $s;
+    $result = curl_exec($curl_request);
+    curl_close($curl_request);
+    return $result;
 }
 
 function fetch_json($url) {
-    $s = fetch($url);
-    return json_decode($s);
+    $result = fetch($url);
+    return json_decode($result);
 }
 
-function dump_json($j) {
-    echo json_encode($j, JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE);
+function dump_json($json) {
+    echo json_encode($json, JSON_PRETTY_PRINT + JSON_UNESCAPED_SLASHES + JSON_UNESCAPED_UNICODE);
 }
 
-// silverstripe/admin => silverstripe/silverstripe-admin
 function packagist_to_github($packagist) {
     list($account, $repo) = explode('/', $packagist);
     if ($account == 'silverstripe') {
-        if (strpos($repo, 'recipe') !== 0 && $repo != 'comment-notifications' && $repo != 'vendor-plugin') {
+        if (strpos($repo, 'recipe') !== 0 && $repo != 'comment-notifications' && $repo != 'vendor-plugin' && $repo != 'eslint-config') {
             $repo = 'silverstripe-' . $repo;
         }
+    }
+    if ($account == 'colymba') {
+        $repo = 'GridfieldBulkEditingTools';
     }
     if ($account == 'cwp') {
         $account = 'silverstripe';
@@ -63,19 +69,96 @@ function packagist_to_github($packagist) {
     return "$account/$repo";
 }
 
-// put in your data here
-$data = [
-    'silverstripe/admin'
+function getComposerRequire($composer) {
+    return $composer["require"];
+}
+
+function outputFormat($name, $version, $composer) {
+    return [
+        'module' => $name,
+        'last_branch' => $version,
+        'composer' => getComposerRequire($composer),
+    ];
+}
+
+function identifyLastBranch($array) {
+    $currentLast = 0;
+    foreach($array as $key => $val) {
+        foreach($val as $k => $v) {
+            if ($k != 'name') {
+                continue;
+            }
+            $version = (float) $v;
+            if ($version > 0) {
+                $currentLast = $version < $currentLast ? $currentLast : $version;
+            }
+        }
+    }
+
+    return $currentLast;
+}
+
+$modules = [
+    'colymba/gridfield-bulk-editing-tools',
+    // 'silverstripe/eslint-config',
+    // 'silverstripe/recipe-ccl',
+    // 'silverstripe/recipe-cms',
+    // 'silverstripe/recipe-collaboration',
+    // 'silverstripe/recipe-content-blocks',
+    // 'silverstripe/recipe-core',
+    // 'silverstripe/recipe-form-building',
+    // 'silverstripe/recipe-plugin',
+    // 'silverstripe/recipe-reporting-tools',
+    // 'silverstripe/recipe-services',
+    // 'silverstripe/recipe-solr-search',
+    // 'silverstripe/registry',
+    // 'silverstripe/reports',
+    // 'silverstripe/restfulserver',
+    // 'silverstripe/securityreport',
+    // 'silverstripe/segment-field',
+    // 'silverstripe/sharedraftcontent',
+    // 'silverstripe/siteconfig',
+    // 'silverstripe/sitewidecontent-report',
+    // 'silverstripe/spamprotection',
+    // 'silverstripe/sqlite3',
+    // 'silverstripe/sspak',
+    // 'silverstripe/staticpublishqueue',
+    // 'silverstripe/subsites',
+    // 'silverstripe/tagfield',
+    // 'silverstripe/taxonomy',
+    // 'silverstripe/textextraction',
+    // 'silverstripe/userforms',
+    // 'silverstripe/vendor-plugin',
+    // 'silverstripe/versioned',
+    // 'silverstripe/versioned-admin',
+    // 'silverstripe/versionfeed',
+    // 'silverstripe/widgets',
+    // 'silverstripe/webpack-config',
+    // 'silverstripe-themes/simple',
+    // 'symbiote/silverstripe-advancedworkflow',
+    // 'symbiote/silverstripe-gridfieldextensions',
+    // 'symbiote/silverstripe-multivaluefield',
+    // 'symbiote/silverstripe-queuedjobs',
+    // 'tractorcow/classproxy',
+    // 'tractorcow/silverstripe-fluent',
+    // 'tractorcow/silverstripe-proxy-db',
+    // 'undefinedoffset/sortablegridfield',
+    // 'silverstripe/mfa',
+    // 'silverstripe/totp-authenticator',
+    // 'silverstripe/webauthn-authenticator',
+    // 'silverstripe/login-forms',
+    // 'silverstripe/security-extensions',
 ];
 
-foreach ($data as $packagist) {
+foreach ($modules as $packagist) {
     $github = packagist_to_github($packagist);
 
     // fetch data from github API
-    // $j = fetch_json("https://api.github.com/repos/$github/branches");
-    // dump_json($j);
+    $json = fetch_json("https://api.github.com/repos/$github/branches");
+    $lastVersion = identifyLastBranch($json);
 
-    // fetch raw file from github
-    $j = fetch_json("https://raw.githubusercontent.com/$github/1/composer.json");
-    dump_json($j);
+    //fetch raw file from github
+    $j = fetch_json("https://raw.githubusercontent.com/$github/$lastVersion/composer.json");
+    
+    dump_json(outputFormat($github, $lastVersion, $j));
 }
